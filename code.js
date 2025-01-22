3510d24f36737db0e13eeabd84e4300123a79a68
Justin Vittitoe <justinvittitoe@Justins-MacBook-Air-2.local>
2025-01-21 18:06:24 -0700

    script for diet selections

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 //querySelector
-// document.querySelector();
-// document.querySelector();
-// document.querySelector();
+const goalbtn = document.querySelector('#goal');
+const restbtn = document.querySelector('#rest');
+const mdbtn = document.querySelector('#md');
+const agebtn = document.querySelector('');
+const submit = document.querySelector('')
 
 
 //Prefrence choices
@@ -11,28 +13,55 @@ const args = {
     md: ['celiac','lactose','diabetes','hypertension','high cholesterol','kidney disease','ibs', 'crohns'],
     age: ['0-30','31-50','50+']
 }
-//Diets
-const diet = {
-    mediterranean: {
+//Diets Array
+const diet = [
+    mediterranean = {
         primary: [args.goal[1],args.goal[4],args.goal[8],args.rest[10],args.md[2],args.md[4],args.md[6],args.age[0],args.age[1],args.age[2]],
         rank: 0
     },
-    keto: {
+    keto = {
         primary: [args.goal[0],args.goal[2],args.goal[4],args.goal[5],args.goal[6],args.rest[0],args.rest[5],args.md[0],args.md[7],args.age[1]],
         rank: 0
     },
-    paleo: {
+    paleo = {
         primary: [args.goal[0],args.goal[1],args.goal[2],args.goal[3],args.goal[5],args.rest[0],args.rest[1],args.rest[6],args.md[0],args.md[7],args.age[1]],
         rank: 0
     }
+]
+console.log(diet)
+
+//Get user selections
+function getUserSelection(event) {
+    event.preventDefault();
+    const userSelection = {
+        goal: '',
+        rest: '',
+        md: '',
+        age: ''
+    }
+    userSelection.goal = document.getElementById('goal').value;
+    userSelection.rest = document.getElementById('rest').value;
+    userSelection.md = document.getElementById('md').value;
+    userSelection.age = document.getElementById('age').value;
+    console.log(userSelection)
+    //save to local storage
+    localStorage.setItem('userSelection',JSON.stringify(userSelection));
 }
-//user selection
-const userSelection = {
-    goal: '',
-    rest: '',
-    md: '',
-    age: ''
+//Event Listener for User Selections
+submit.addEventListener('click',getUserSelection)
+
+//retrieve user selection and rank diets
+function getUserSelectionFromLocalStorage() {
+    const storedSelection = localStorage.getItem('userSelection');
+    if (storedSelection) {
+         
+    }
+    return null;
 }
+
+
+
+
 
 
 
@@ -62,12 +91,8 @@ function cartesian(args) {
 }
 
 const allCombinations = cartesian(args);
-console.log(allCombinations);
 const r = [allCombinations]
 console.log(r);
 
-function primaryDiet(args,diet) {
-    console.log(allCombinations.indexOf(['fiber','GF','celiac','0-30']))
-}
-primaryDiet()
+
 
