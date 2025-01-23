15b104417385d0d106354fcc2c37c2729d76d172
Justin Vittitoe <justinvittitoe@Justins-MacBook-Air-2.local>
2025-01-22 19:21:49 -0700

    ranking for diets

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ const args = {
 //Diets Array
 const diet = {
     mediterranean: {
-        primary: [args.goal[1],args.goal[4],args.goal[8],args.rest[10],args.md[2],args.md[4],args.md[6],args.age[0],args.age[1],args.age[2]],
+        primary: [args.goal[1],args.rest[10],args.md[2],args.age[0],],
         rank: 0
     },
     keto: {
-        primary: [args.goal[0],args.goal[2],args.goal[4],args.goal[5],args.goal[6],args.rest[0],args.rest[5],args.md[0],args.md[7],args.age[1]],
+        primary: [args.goal[0],args.rest[0],args.md[7],args.age[1]],
         rank: 0
     },
     paleo: {
-        primary: [args.goal[0],args.goal[1],args.goal[2],args.goal[3],args.goal[5],args.rest[0],args.rest[1],args.rest[6],args.md[0],args.md[7],args.age[1]],
+        primary: [args.goal[2],args.rest[1],args.md[0],args.age[1]],
         rank: 0
     }
 }
@@ -60,28 +60,56 @@ function getUserSelection(event) {
     //save to local storage
     localStorage.setItem('userSelection',JSON.stringify(userSelection));
     
-    for (i=0; i < 2; i++) {
+    for (i=0; i < 4; i++) {
         if (diet.mediterranean.primary[i] === userSelection.goal) {
             diet.mediterranean.rank += 3;
         }
-        if (diet.mediterranean.primary[3] === userSelection.rest) {
+        if (diet.mediterranean.primary[i] === userSelection.rest) {
             diet.mediterranean.rank += 1;
         }
-    }
-    for (i=5; i < 7; i++) {
         if (diet.mediterranean.primary[i]===userSelection.md) {
-            diet.mediterranean.rank += 0;
+            diet.mediterranean.rank += 1;
         }
-    }
-    
-    for (i=7; i<10;i++) {
         if (diet.mediterranean.primary[i]===userSelection.age) {
             diet.mediterranean.rank += 1;
     }
     }
-     
- 
-    console.log(diet.mediterranean.rank)
+    for (i=0; i < 4; i++) {
+        if (diet.keto.primary[i] === userSelection.goal) {
+            diet.keto.rank += 3;
+        }
+        if (diet.keto.primary[i] === userSelection.rest) {
+            diet.keto.rank += 1;
+        }
+        if (diet.keto.primary[i]===userSelection.md) {
+            diet.keto.rank += 1;
+        }
+        if (diet.keto.primary[i]===userSelection.age) {
+            diet.keto.rank += 1;
+    }
+    }
+    for (i=0; i < 4; i++) {
+        if (diet.paleo.primary[i] === userSelection.goal) {
+            diet.paleo.rank += 3;
+        }
+        if (diet.paleo.primary[i] === userSelection.rest) {
+            diet.paleo.rank += 1;
+        }
+        if (diet.paleo.primary[i]===userSelection.md) {
+            diet.paleo.rank += 1;
+        }
+        if (diet.paleo.primary[i]===userSelection.age) {
+            diet.paleo.rank += 1;
+    }
+    } 
+
+    //sort diets by rank
+    const sortedDiets = Object.keys(diet).sort((a,b) => diet[b].rank - diet[a].rank);
+    console.log(sortedDiets);
+    //display top diet
+    const selectedDiet = sortedDiets[0];
+    console.log(selectedDiet);
+    localStorage.setItem('topDiet',JSON.stringify(selectedDiet));
 } 
     
 
